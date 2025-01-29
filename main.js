8fad0dc1e98597dc03730fc78f9c7338f11a11e3
drshxs <104319119+drshxs@users.noreply.github.com>
2025-01-29 14:49:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ window.onload = function() {
     const spanElement = document.querySelector('.first');
     if (spanElement) {
       spanElement.innerText = 'backwoods was here';
+      tippy(spanElement, {
+        content: 'This is a tooltip!',
+      });
     } else {
       console.warn('Element with class "first" not found!');
     }
-  }, 1500); 
+  }, 1500);
 };
