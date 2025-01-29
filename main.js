fb083b17c7b361f8f2f0ce0d65ac80a357411954
drshxs <104319119+drshxs@users.noreply.github.com>
2025-01-29 14:47:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const spanElement = document.querySelector('.first');
-if (spanElement) {
-  spanElement.innerText = 'backwoods was here';
-}
+window.onload = function() {
+  setTimeout(function() {
+    const spanElement = document.querySelector('.first');
+    if (spanElement) {
+      spanElement.innerText = 'backwoods was here';
+    } else {
+      console.warn('Element with class "first" not found!');
+    }
+  }, 1500); 
+};
